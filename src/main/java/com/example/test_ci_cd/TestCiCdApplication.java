1bc8667b46bfdf7a9c7285e134ee0d73ceac5803
Boburjon <soliyevboburjon95@gmail.com>
2024-06-19 08:18:40 +0500

    Init

---
+++ src/main/java/com/example/test_ci_cd/TestCiCdApplication.java
@@ -0,0 +1,19 @@
+package com.example.test_ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class TestCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCiCdApplication.class, args);
+	}
+	@GetMapping
+	public String hello(){
+		return "hello v1";
+	}
+}
